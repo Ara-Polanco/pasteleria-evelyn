1bb56acca693a3c228c4f6e9e3983b953980e369
Araceli Polanco <120612439+Ara-Polanco@users.noreply.github.com>
2023-12-06 20:00:57 -0600

    Adding: slider to main section

---
+++ js/app.js
@@ -0,0 +1,27 @@
+
+
+
+
+window.swiper = new Swiper( {
+    el: '.galeria',
+    slideClass: 'galeria__categoria',
+    createElements: true,
+    autoplay: {
+        delay: 5000
+    },
+    loop: true,
+    spaceBetween: 50,
+    pagination: true,
+    navigation: true
+/* 
+    autoplay: {
+        delete: 5000
+    },
+    spaceBetween:70,
+    loop: true,
+    pagination: true,
+    navigation: true
+     */
+})
+
+
